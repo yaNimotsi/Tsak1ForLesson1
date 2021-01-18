c4225b6a370f4ddbdccd61b0b87f2698a29fb3b0
yaNimotsi <yanimotsi@yanmdex.ru>
2021-01-18 22:09:43 +0300

    Add project files.

---
+++ Tsak1ForLesson1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tsak1ForLesson1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var dToday = DateTime.Now.ToString("D");
+
+            Console.Write("Укажите свое имя: ");
+            var userName = Console.ReadLine();
+
+            Console.WriteLine("Привет {0}, сегодня {1}", userName, dToday);
+            Console.WriteLine($"Привет {userName}, сегодня {dToday}");
+
+            Console.ReadLine();
+        }
+    }
+}
